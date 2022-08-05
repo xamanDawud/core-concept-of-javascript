17d39dce876dd9132f456c10ca00cbcf6597d9a4
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-05 08:37:49 +0600

    var vs let vs const, explore done

---
+++ var-let-const.js
@@ -0,0 +1,10 @@
+var age = 20;
+age = 30;
+
+const name = "Xaman dawud";
+// name = "Salman khan"; //  Error khaba mama sure thako
+// console.log(name);
+
+let book = "Do bright your Future with the hard work";
+var book = "ki obsta mama";
+console.log(book);
