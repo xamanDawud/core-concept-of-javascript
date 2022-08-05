3c11d7e1128b1ac4d89a3ed71dec52287cda88f0
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-05 08:33:11 +0600

    recap done, onek kisu janih ekn Yoyo

--- recap.js
+++ recap.js
@@ -1,1 +1,49 @@
-console.log("Hello xaman dawud");
+// 1. Variable Declaration
+var desc = "This product is very smooth and comfortable";
+
+// math operation: =,+,-,*, /
+// shorthand: ++ --
+
+// 2. Array
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var thirdFriends = arr[2];
+arr[3] = 44;
+
+// 3. Conditional
+// < , > , <= , >= , == , ===, != , !== , && , or
+if (girlFriends < 2) {
+    console.log("jobne korlata ki mama..!");
+} else {
+    console.log("Tmi mama kamla...");
+}
+
+// 4. Loop
+var i = 0;
+
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// 5. Function
+function BabuUtse(time) {
+    if (time < 9) {
+        console.log("Babu Ekono gumacche");
+    } else {
+        console.log("Babu uthe poreche");
+    }
+}
+
+// 6. Object
+
+var mobile = {
+    name: "Samsung",
+    model: "J4+",
+    price: 20000,
+    camera: "10MP",
+};
